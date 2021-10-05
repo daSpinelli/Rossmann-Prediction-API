3209e17125548075798bfe036fc47476fa3517b5
daspinelli <denny.dealmeida@gmail.com>
2021-10-05 20:40:18 -0300

    FIT TRANSFORM changed to TRANSFORM - avoid data leak

--- aux_class/Rossmann.py
+++ aux_class/Rossmann.py
@@ -7,7 +7,7 @@ import datetime
 
 class Rossmann(object):
     def __init__(self):
-        self.home_path='/home/bem/repos/rossmann_sales_predict/rossmann_sales_predict/'
+        self.home_path=''
         self.competition_distance_scaler = pickle.load(open(self.home_path + 'asset/pickle/competition_distance_scaler.pkl', 'rb'))
         self.competition_time_month_scaler = pickle.load(open(self.home_path + 'asset/pickle/competition_time_month_scaler.pkl', 'rb'))
         self.promo_time_week_scaler = pickle.load(open(self.home_path + 'asset/pickle/promo_time_week_scaler.pkl', 'rb'))
